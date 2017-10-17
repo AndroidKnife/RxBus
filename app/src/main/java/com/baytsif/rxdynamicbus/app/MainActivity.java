6ecf920a9f442b2f72d521ad12c58e14750807c2
baytsif <oressel@gmail.com>
2017-10-17 15:53:01 +0300

    test

--- app/src/main/java/com/baytsif/rxdynamicbus/app/MainActivity.java
+++ app/src/main/java/com/baytsif/rxdynamicbus/app/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
