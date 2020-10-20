66133ca8b4a749f31632019260420fb2c81ec4ac
henjue <henjue@gmail.com>
2020-10-20 17:21:21 +0800

    Migrate to AndroidX
    minSdkVersion:14
    targetSdkVersion:30
    compileSdkVersion:30
    buildToolsVersion:30.0.2

--- app/src/main/java/com/hwangjr/rxbus/app/MainActivity.java
+++ app/src/main/java/com/hwangjr/rxbus/app/MainActivity.java
@@ -1,10 +1,10 @@
 package com.hwangjr.rxbus.app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
--- rxbus/src/main/java/com/hwangjr/rxbus/RxBusOlder.java
+++ rxbus/src/main/java/com/hwangjr/rxbus/RxBusOlder.java
@@ -1,6 +1,6 @@
 package com.hwangjr.rxbus;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
